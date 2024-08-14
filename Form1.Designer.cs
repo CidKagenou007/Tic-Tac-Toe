fed6658eac5c1ba823379a8feb67f3f785403829
Amir Abdeldjalil <kzero3187@gmail.com>
2024-08-14 23:05:27 +0100

    End Work

---
+++ Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Tic_Tac_Toe_Game
+{
+    partial class FmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FmMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbPlayer = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbWinner = new System.Windows.Forms.Label();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bernard MT Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(35, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Restart Game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bernard MT Condensed", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(462, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 57);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tic-Tac-Toe Game";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bernard MT Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(86, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Turn";
+            // 
+            // lbPlayer
+            // 
+            this.lbPlayer.AutoSize = true;
+            this.lbPlayer.Font = new System.Drawing.Font("Bernard MT Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPlayer.ForeColor = System.Drawing.Color.White;
+            this.lbPlayer.Location = new System.Drawing.Point(68, 179);
+            this.lbPlayer.Name = "lbPlayer";
+            this.lbPlayer.Size = new System.Drawing.Size(123, 38);
+            this.lbPlayer.TabIndex = 3;
+            this.lbPlayer.Text = "Player  1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bernard MT Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(86, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 38);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Winner";
+            // 
+            // lbWinner
+            // 
+            this.lbWinner.AutoSize = true;
+            this.lbWinner.Font = new System.Drawing.Font("Bernard MT Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbWinner.ForeColor = System.Drawing.Color.Lime;
+            this.lbWinner.Location = new System.Drawing.Point(68, 309);
+            this.lbWinner.Name = "lbWinner";
+            this.lbWinner.Size = new System.Drawing.Size(155, 38);
+            this.lbWinner.TabIndex = 5;
+            this.lbWinner.Tag = "0";
+            this.lbWinner.Text = "In Progress";
+            // 
+            // pb9
+            // 
+            this.pb9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb9.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pb9.Location = new System.Drawing.Point(809, 377);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(74, 84);
+            this.pb9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb9.TabIndex = 14;
+            this.pb9.TabStop = false;
+            this.pb9.Tag = "0";
+            this.pb9.Click += new System.EventHandler(this.pb9_Click);
+            // 
+            // pb8
+            // 
+            this.pb8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb8.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pb8.Location = new System.Drawing.Point(611, 377);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(74, 84);
+            this.pb8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb8.TabIndex = 13;
+            this.pb8.TabStop = false;
+            this.pb8.Tag = "0";
+            this.pb8.Click += new System.EventHandler(this.pb8_Click);
+            // 
+            // pb7
+            // 
+            this.pb7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb7.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pb7.Location = new System.Drawing.Point(425, 377);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(74, 84);
+            this.pb7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb7.TabIndex = 12;
+            this.pb7.TabStop = false;
+            this.pb7.Tag = "0";
+            this.pb7.Click += new System.EventHandler(this.pb7_Click);
+            // 
+            // pb6
+            // 
+            this.pb6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb6.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pb6.Location = new System.Drawing.Point(809, 228);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(74, 84);
+            this.pb6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb6.TabIndex = 11;
+            this.pb6.TabStop = false;
+            this.pb6.Tag = "0";
+            this.pb6.Click += new System.EventHandler(this.pb6_Click);
+            // 
+            // pb5
+            // 
+            this.pb5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb5.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pb5.Location = new System.Drawing.Point(611, 228);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(74, 84);
+            this.pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb5.TabIndex = 10;
+            this.pb5.TabStop = false;
+            this.pb5.Tag = "0";
+            this.pb5.Click += new System.EventHandler(this.pb5_Click);
+            // 
+            // pb4
+            // 
+            this.pb4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb4.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pb4.Location = new System.Drawing.Point(425, 228);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(74, 84);
+            this.pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb4.TabIndex = 9;
+            this.pb4.TabStop = false;
+            this.pb4.Tag = "0";
+            this.pb4.Click += new System.EventHandler(this.pb4_Click);
+            // 
+            // pb3
+            // 
+            this.pb3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb3.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pb3.Location = new System.Drawing.Point(809, 95);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(74, 84);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb3.TabIndex = 8;
+            this.pb3.TabStop = false;
+            this.pb3.Tag = "0";
+            this.pb3.Click += new System.EventHandler(this.pb3_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb2.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pb2.Location = new System.Drawing.Point(611, 95);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(74, 84);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb2.TabIndex = 7;
+            this.pb2.TabStop = false;
+            this.pb2.Tag = "0";
+            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb1.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pb1.Location = new System.Drawing.Point(425, 95);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(74, 84);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 6;
+            this.pb1.TabStop = false;
+            this.pb1.Tag = "0";
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // FmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(945, 633);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.lbWinner);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbPlayer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FmMain";
+            this.Text = "Tic-Tac-Toe Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FmMain_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbPlayer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbWinner;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tic_Tac_Toe_Game.Properties;
+
+namespace Tic_Tac_Toe_Game
+{
+    public partial class FmMain : Form
+    {
+        public FmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FmMain_Paint(object sender, PaintEventArgs e)
+        {
+            Color White = Color.White;
+            Pen Pen = new Pen(White,10);
+
+            Pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            Pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            e.Graphics.DrawLine(Pen, 550 , 90, 550 , 480);
+            e.Graphics.DrawLine(Pen, 755, 90, 755, 480);
+            e.Graphics.DrawLine(Pen, 400, 200, 900, 200);
+            e.Graphics.DrawLine(Pen, 400, 350, 900, 350);
+
+
+        }
+
+        void Restart()
+        {
+
+            pb1.Image = Resources.question_mark_96;
+            pb2.Image = Resources.question_mark_96;
+            pb3.Image = Resources.question_mark_96;
+            pb4.Image = Resources.question_mark_96;
+            pb5.Image = Resources.question_mark_96;
+            pb6.Image = Resources.question_mark_96;
+            pb7.Image = Resources.question_mark_96;
+            pb8.Image = Resources.question_mark_96;
+            pb9.Image = Resources.question_mark_96;
+
+            pb1.Tag = 0;
+            pb2.Tag = 0;
+            pb3.Tag = 0;
+            pb4.Tag = 0;
+            pb5.Tag = 0;
+            pb6.Tag = 0;
+            pb7.Tag = 0;
+            pb8.Tag = 0;
+            pb9.Tag = 0;
+
+            lbWinner.Tag = 0;
+
+            pb1.BackColor = Color.Black;
+            pb2.BackColor = Color.Black;
+            pb3.BackColor = Color.Black;
+            pb4.BackColor = Color.Black;
+            pb5.BackColor = Color.Black;
+            pb6.BackColor = Color.Black;
+            pb7.BackColor = Color.Black;
+            pb8.BackColor = Color.Black;
+            pb9.BackColor = Color.Black;
+
+
+            lbPlayer.Text = "Player  1" ;
+            lbWinner.Text = "In Progress";
+
+        }
+
+        bool IsGameOver()
+        {
+            return ((Convert.ToInt16(pb1.Tag) != 0) && (Convert.ToInt16(pb2.Tag) != 0) && (Convert.ToInt16(pb3.Tag) != 0) &&
+                (Convert.ToInt16(pb4.Tag) != 0) && (Convert.ToInt16(pb5.Tag) != 0) && (Convert.ToInt16(pb6.Tag) != 0) &&
+                (Convert.ToInt16(pb7.Tag) != 0) && (Convert.ToInt16(pb8.Tag) != 0) && (Convert.ToInt16(pb9.Tag) != 0));
+        }
+
+        void CheckWinner(short Index)
+        {
+
+            if (Convert.ToInt16(pb1.Tag) == Index && Convert.ToInt16(pb2.Tag) == Index &&
+                Convert.ToInt16(pb3.Tag) == Index)
+            {
+                pb1.BackColor = Color.Green;
+                pb2.BackColor = Color.Green;
+                pb3.BackColor = Color.Green;
+                lbWinner.Tag = Index;
+                return;
+            }
+
+            if ((Convert.ToInt16(pb1.Tag) == Index && Convert.ToInt16(pb4.Tag) == Index &&
+                Convert.ToInt16(pb7.Tag) == Index))
+            {
+                pb1.BackColor = Color.Green;
+                pb4.BackColor = Color.Green;
+                pb7.BackColor = Color.Green;
+                lbWinner.Tag = Index;
+                return;
+            }
+
+            if (Convert.ToInt16(pb2.Tag) == Index && Convert.ToInt16(pb5.Tag) == Index &&
+                Convert.ToInt16(pb8.Tag) == Index)
+            {
+                pb2.BackColor = Color.Green;
+                pb5.BackColor = Color.Green;
+                pb8.BackColor = Color.Green;
+                lbWinner.Tag = Index;
+                return;
+            }
+
+            if (Convert.ToInt16(pb3.Tag) == Index && Convert.ToInt16(pb6.Tag) == Index &&
+                Convert.ToInt16(pb9.Tag) == Index)
+            {
+                pb3.BackColor = Color.Green;
+                pb6.BackColor = Color.Green;
+                pb9.BackColor = Color.Green;
+                lbWinner.Tag = Index;
+                return;
+            }
+
+            if (Convert.ToInt16(pb1.Tag) == Index && Convert.ToInt16(pb5.Tag) == Index &&
+                Convert.ToInt16(pb9.Tag) == Index)
+            {
+                pb1.BackColor = Color.Green;
+                pb5.BackColor = Color.Green;
+                pb9.BackColor = Color.Green;
+                lbWinner.Tag = Index;
+                return;
+            }
+
+            if (Convert.ToInt16(pb3.Tag) == Index && Convert.ToInt16(pb5.Tag) == Index &&
+                Convert.ToInt16(pb7.Tag) == Index) {
+
+                pb3.BackColor = Color.Green;
+                pb5.BackColor = Color.Green;
+                pb7.BackColor = Color.Green;
+                lbWinner.Tag = Index;
+                return;
+            }
+
+            if (Convert.ToInt16(pb4.Tag) == Index && Convert.ToInt16(pb5.Tag) == Index &&
+                Convert.ToInt16(pb6.Tag) == Index)
+            {
+                pb4.BackColor = Color.Green;
+                pb5.BackColor = Color.Green;
+                pb6.BackColor = Color.Green;
+                lbWinner.Tag = Index;
+                return;
+            }
+
+            if (Convert.ToInt16(pb7.Tag) == Index && Convert.ToInt16(pb8.Tag) == Index &&
+                Convert.ToInt16(pb9.Tag) == Index)
+            {
+                pb7.BackColor = Color.Green;
+                pb8.BackColor = Color.Green;
+                pb9.BackColor = Color.Green;
+                lbWinner.Tag = Index;
+                return;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Restart();
+        }
+
+        void UpdateGame(object sender)
+        {
+
+            if (Convert.ToInt16(((PictureBox)sender).Tag) == 1 || Convert.ToInt16(((PictureBox)sender).Tag) == 2)
+                MessageBox.Show("Wrong Choice", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                if (lbPlayer.Text == "Player  1")
+                {
+                    ((PictureBox)sender).Image = Resources.X;
+                    ((PictureBox)sender).Tag = 1;
+
+                    CheckWinner(1);
+
+                    if (Convert.ToInt16(lbWinner.Tag) == 1)
+                    {
+                        lbWinner.Text = "Player  1";
+                        lbPlayer.Text = "Game Over";
+                        MessageBox.Show("Game Over", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+
+                    lbPlayer.Text = "Player  2";
+
+
+                }
+
+                else
+                {
+                    ((PictureBox)sender).Image = Resources.O;
+                    ((PictureBox)sender).Tag = 2;
+
+                    CheckWinner(2);
+
+                    if (Convert.ToInt16(lbWinner.Tag) == 2)
+                    {
+                        lbWinner.Text = "Player  2";
+                        lbPlayer.Text = "Game Over";
+                        MessageBox.Show("Game Over", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+
+                    lbPlayer.Text = "Player  1";
+                }
+            }
+
+            if (IsGameOver())
+            {
+                lbWinner.Text = "Draw";
+                lbPlayer.Text = "Game Over";
+                MessageBox.Show("Game Over", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+
+        private void pb1_Click(object sender, EventArgs e)
+        {
+            UpdateGame(sender);
+        }
+
+        private void pb2_Click(object sender, EventArgs e)
+        {
+            UpdateGame(sender);
+        }
+
+        private void pb3_Click(object sender, EventArgs e)
+        {
+            UpdateGame(sender);
+        }
+
+        private void pb4_Click(object sender, EventArgs e)
+        {
+            UpdateGame(sender);
+        }
+
+        private void pb5_Click(object sender, EventArgs e)
+        {
+            UpdateGame(sender);
+        }
+
+        private void pb6_Click(object sender, EventArgs e)
+        {
+            UpdateGame(sender);
+        }
+
+        private void pb7_Click(object sender, EventArgs e)
+        {
+            UpdateGame(sender);
+        }
+
+        private void pb8_Click(object sender, EventArgs e)
+        {
+            UpdateGame(sender);
+        }
+
+        private void pb9_Click(object sender, EventArgs e)
+        {
+            UpdateGame(sender);
+        }
+    }
+}
